d7bd0060e6782393525cff567fd7011a2d95f847
ryan <ryan.jentzsch@gmail.com>
2022-01-08 04:06:13 -0700

    🌀 refactor Fleshed out the error handling
    - Show a notification when an error occurs
    - Put the error details in the console log

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -1,4 +1,5 @@
-import PinManager, {IPinData} from "api/managers/PinManager";
+import PinManager, {IPinData} from 'api/managers/PinManager';
+import {KIND, Notification} from 'baseui/notification';
 import PinPage from 'Pages/PinPage';
 import SignaturePad from 'Pages/SignaturePad';
 import SignaturePage from 'Pages/SignaturePage';
@@ -19,6 +20,7 @@ const LandingPage = (props: IProps) => {
     const [isSignaturePadOpen, setIsSignaturePadOpen] = useState(false);
     const [pinValues, setPinValues] = useState([...defaultPinValues]);
     const [pinData, setPinData] = useState<IPinData | null>(null);
+    const [errorDetails, setErrorDetails] = useState<null | unknown>(null);
 
     /**
      * This fires when the user closes the signature modal
@@ -26,43 +28,44 @@ const LandingPage = (props: IProps) => {
      */
     const handleSignatureModalClose = (signature: string | null) => {
         const saveSignature = async (signatureImage: string) => {
-            alert('Signature Image Len: ' + signatureImage.length);
             if (pinData) {
                 const pinInfo = {...pinData.pin_info};
                 pinInfo.Image = signatureImage;
                 const pinResponse = await pinManager.update(pinInfo);
+                setIsSignaturePadOpen(false);
                 if (pinResponse.success) {
-                    setIsSignaturePadOpen(false);
                     setPinValues([...defaultPinValues]);
                     setPinData(null);
                 } else {
-                    // todo: display actual soft error or log it or do something when failure occurs
                     console.log('pinResponse', pinResponse);
-                    alert('Soft Error: ' + JSON.stringify(pinResponse));
+                    setErrorDetails(pinResponse);
                 }
             }
-        }
+        };
 
-        if (signature) saveSignature(signature)
+        if (signature) saveSignature(signature);
     };
+
+    const errorNotification = (
+        <Notification kind={KIND.warning}>
+            An error has occurred. Check the internet connection or console log
+        </Notification>
+    );
+
+    const normal = (
+        <>
+            {pinData === null ? (
+                <PinPage onPinEntered={(pd) => setPinData(pd)} pinValues={pinValues} pinManager={pinManager} />
+            ) : (
+                <SignaturePage pinData={pinData} onShowSignatureModal={() => setIsSignaturePadOpen(true)} />
+            )}
+        </>
+    );
 
     return (
         <>
             <div className="neu-main">
-                <div className="neu-content">
-                    {pinData === null ? (
-                        <PinPage
-                            onPinEntered={(pd) => setPinData(pd)}
-                            pinValues={pinValues}
-                            pinManager={pinManager}
-                        />
-                    ) : (
-                        <SignaturePage
-                            pinData={pinData}
-                            onShowSignatureModal={() => setIsSignaturePadOpen(true)}
-                        />
-                    )}
-                </div>
+                <div className="neu-content">{errorDetails === null ? normal : errorNotification}</div>
             </div>
 
             <SignaturePad
